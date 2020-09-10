cb235a13393e695af8d8a7f5c509eec2f618708e
ravikatrenikona <57341266+ravikatrenikona@users.noreply.github.com>
2020-09-10 09:24:17 +0530

    Create ExportSolution1.ps1

---
+++ BuildTools/Xrm.CI.Framework.BuildTools/Lib/xRMCIFramework/9.0.0/ExportSolution1.ps1
@@ -0,0 +1,91 @@
+﻿#
+# ExportSolution.ps1
+#
+
+param(
+[string]$CrmConnectionString,
+[string]$SolutionName,
+[bool]$ExportManaged,
+[bool]$ExportUnmanaged = $true,
+[string]$TargetVersion,
+[string]$ExportSolutionOutputPath,
+[bool]$UpdateVersion,
+[string]$RequiredVersion,
+[int]$Timeout,
+[bool]$ExportIncludeVersionInSolutionName,
+[bool]$ExportAutoNumberingSettings,
+[bool]$ExportCalendarSettings,
+[bool]$ExportCustomizationSettings,
+[bool]$ExportEmailTrackingSettings,
+[bool]$ExportExternalApplications,
+[bool]$ExportGeneralSettings,
+[bool]$ExportIsvConfig,
+[bool]$ExportMarketingSettings,
+[bool]$ExportOutlookSynchronizationSettings,
+[bool]$ExportRelationshipRoles,
+[bool]$ExportSales
+)
+
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+
+Write-Verbose 'Entering ExportSolution.ps1'
+
+#Parameters
+Write-Verbose "CrmConnectionString = $CrmConnectionString"
+Write-Verbose "SolutionName = $SolutionName"
+Write-Verbose "ExportManaged = $ExportManaged"
+Write-Verbose "ExportUnmanaged = $ExportUnmanaged"
+Write-Verbose "TargetVersion = $TargetVerion"
+Write-Verbose "ExportSolutionOutputPath = $ExportSolutionOutputPath"
+Write-Verbose "UpdateVersion = $UpdateVersion"
+Write-Verbose "RequiredVersion = $RequiredVersion"
+Write-Verbose "Timeout = $Timeout"
+Write-Verbose "ExportIncludeVersionInSolutionName = $ExportIncludeVersionInSolutionName"
+Write-Verbose "ExportAutoNumberingSettings = $ExportAutoNumberingSettings"
+Write-Verbose "ExportCalendarSettings = $ExportCalendarSettings"
+Write-Verbose "ExportCustomizationSettings = $ExportCustomizationSettings"
+Write-Verbose "ExportEmailTrackingSettings = $ExportEmailTrackingSettings"
+Write-Verbose "ExportExternalApplications = $ExportExternalApplications"
+Write-Verbose "ExportGeneralSettings = $ExportGeneralSettings"
+Write-Verbose "ExportIsvConfig = $ExportIsvConfig"
+Write-Verbose "ExportMarketingSettings = $ExportMarketingSettings"
+Write-Verbose "ExportOutlookSynchronizationSettings = $ExportOutlookSynchronizationSettings"
+Write-Verbose "ExportRelationshipRoles = $ExportRelationshipRoles"
+Write-Verbose "ExportSales = $ExportSales"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Write-Verbose "Script Path: $scriptPath"
+
+#Load XrmCIFramework
+$xrmCIToolkit = $scriptPath + "\Xrm.Framework.CI.PowerShell.Cmdlets.dll"
+Write-Verbose "Importing CIToolkit: $xrmCIToolkit" 
+Import-Module $xrmCIToolkit
+Write-Verbose "Imported CIToolkit"
+
+#Update Version
+if ($UpdateVersion)
+{
+	Write-Host "Updating Solution Version to $RequiredVersion"
+	Set-XrmSolutionVersion -ConnectionString "$CrmConnectionString" -SolutionName $SolutionName -Version $RequiredVersion
+	Write-Host "Solution Version Updated"
+}
+
+#Solution Export
+
+$exportManagedFile
+$exportUnmanagedFile
+
+if ($ExportUnmanaged)
+{
+    Write-Host "Exporting Unmanaged Solution"
+        
+    $exportUnmanagedFile = Export-XrmSolution -ConnectionString "$CrmConnectionString" -UniqueSolutionName $SolutionName -OutputFolder "$ExportSolutionOutputPath" 
+        
+    Write-Host "UnManaged Solution Exported $ExportSolutionOutputPath\$exportUnmanagedFile"
+}
+
+
+
+Write-Verbose 'Leaving ExportSolution.ps1'
